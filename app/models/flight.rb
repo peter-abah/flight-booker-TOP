4c58e00c8157846b2440f16790e23e41717287e2
Peter Abah <peterabah.ob@gmail.com>
2021-07-29 21:17:33 +0100

    create bookings controller

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,8 @@ class Flight < ApplicationRecord
   belongs_to :to_airport, class_name: 'Airport', foreign_key: :to_airport_id
   belongs_to :from_airport, class_name: 'Airport', foreign_key: :from_airport_id
 
+  has_many :bookings
+
   def formatted_date
     date.strftime('%d-%m-%Y')
   end
