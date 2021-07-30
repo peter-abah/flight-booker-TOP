57c97a40736a81e223b853f3e0f549312cb35578
Peter Abah <peterabah.ob@gmail.com>
2021-07-30 13:54:33 +0100

    add show view for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,8 @@
 class BookingsController < ApplicationController
+  def index
+    redirect_to root_path
+  end
+
   def new
     @flight = Flight.find(params[:flight_id])
     @booking = Booking.new
@@ -17,6 +21,12 @@ class BookingsController < ApplicationController
       render :new
     end
   end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  private
 
   def booking_params
     params.require(:booking).permit(:flight_id, passengers_attributes: %i[name email])
