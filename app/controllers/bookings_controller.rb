6758180bb87ef42049202b0eb794959b2110b0c3
Peter Abah <peterabah.ob@gmail.com>
2021-07-30 13:30:47 +0100

    add #create to bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,24 @@
 class BookingsController < ApplicationController
   def new
     @flight = Flight.find(params[:flight_id])
-
     @booking = Booking.new
-    params[:passengers_no].to_i.times { @booking.passengers.build }
+
+    passengers_no = params[:passengers_no].to_i
+    passengers_no.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @flight = Flight.find(@booking.flight.id)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
+  end
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: %i[name email])
   end
 end
