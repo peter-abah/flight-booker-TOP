5f6629a26e63d383ec8a6c37deda0268fb115322
Peter Abah <peterabah.ob@gmail.com>
2021-08-01 10:42:09 +0100

    fix bug in  pasenger mailer

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -2,8 +2,8 @@ class PassengerMailer < ApplicationMailer
   default from: 'peterabah11@gmail.com'
 
   def thank_you_email
-    @passenger = params[:passenger]
-    @url = 'http://localhost:3000'
-    mail(to: @passenger.email, subject: 'Thanks For Booking')
+    @booking = params[:booking]
+    @passengers = @booking.passengers
+    mail(to: @passengers.pluck(:email), subject: 'Thanks For Booking')
   end
 end
