5a9fa04305e3dad93a34cddee0b76411f7309c22
Peter Abah <peterabah.ob@gmail.com>
2021-08-01 10:06:29 +0100

    create email views

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'peterabah11@gmail.com'
+
+  def thank_you_email
+    @passenger = params[:passenger]
+    @url = 'http://localhost:3000'
+    mail(to: @passenger.email, subject: 'Thanks For Booking')
+  end
 end
